a7671c79f03e4a7bc2333fee1ea31632e6ca50c4
Tamás Krizsan <tamas@krizsanmunka.hu>
2024-11-08 21:24:42 +0100

    add form logic

--- src/app/component/contact-form/contact-form.component.ts
+++ src/app/component/contact-form/contact-form.component.ts
@@ -1,27 +1,91 @@
 import { Component } from '@angular/core';
-import {FormsModule} from "@angular/forms";
+import {ReactiveFormsModule, FormBuilder, Validators, FormGroup} from "@angular/forms";
+
+import {NgxMaskDirective} from 'ngx-mask';
+
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
   imports: [
-    FormsModule
+    ReactiveFormsModule,
+    NgxMaskDirective
+
   ],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
 })
+
+
+
+
+
+
 export class ContactFormComponent {
 
 
+    contactForm!: FormGroup;
 
-  inputText: string = '';
+
+    constructor(private formBuilder: FormBuilder) {
+
+      this.contactForm = this.formBuilder.group({
+        name: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        phone: ['', Validators.required],
+        taxId: ['', [Validators.required, Validators.minLength(6)]],
+          message: ['', [Validators.required, Validators.minLength(6)]]
+
+      });
+    }
+
+    onSubmit() {
+      if (this.contactForm.valid) {
+        console.log(this.contactForm.value);
+      }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+  contactForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    phone: new FormControl(''),
+    taxNumber: new FormControl(''),
+    message: new FormControl('')
+
+  });
 
   onSubmit() {
-    alert(`You entered: ${this.inputText}`);
+    // TODO: Use EventEmitter with form value
+    console.warn(this.contactForm.value);
   }
 
+*/
 
 
 
+
+
+
+
+  // inputText: string = '';
+
+  /*onSubmit() {
+    alert(`You entered: ${this.inputText}`);
+  }*/
 
 }
