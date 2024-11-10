c0b3e08fa45c9664198c76fbee2baf4aeeaa9b36
Tamás Krizsan <tamas@krizsanmunka.hu>
2024-11-10 21:09:03 +0100

    update files

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import {provideEnvironmentNgxMask} from "ngx-mask";
+import { provideHttpClient} from '@angular/common/http';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideEnvironmentNgxMask()
+    provideEnvironmentNgxMask(),
+    provideHttpClient()
 
   ]
 };
